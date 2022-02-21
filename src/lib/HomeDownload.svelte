3ec9abfdb73e8fdaacab87c19ca5e73c2b976ad5
Peter Coyne <61690004+petercoyne@users.noreply.github.com>
2022-02-21 20:20:50 +0000

    Abandon 4-col view

--- src/lib/HomeDownload.svelte
+++ src/lib/HomeDownload.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <a href="{url}" class="">
-	<img src="/images/dl-{kind}.webp" alt="Download on {kind}" class="max-h-16 md:max-h-10 2xl:max-h-8"/>
+	<img src="/images/dl-{kind}.webp" alt="Download on {kind}" class="max-h-16 md:max-h-10 lg:max-h-12"/>
 </a>
 
--- src/lib/HomeGrid.svelte
+++ src/lib/HomeGrid.svelte
@@ -1,3 +1,3 @@
-<div class="mx-auto grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-4 p-8 gap-24 md:p-16 md:gap-24 md:max-w-5xl 2xl:max-w-none 2xl:gap-12">
+<div class="mx-auto grid grid-cols-1 md:grid-cols-2 p-8 gap-24 md:p-16 md:gap-24 md:max-w-5xl lg:max-w-6xl">
 	<slot/>
 </div>
