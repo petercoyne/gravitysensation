abdf4a29dce1085fc3cbd072dfca3a31ea7c3604
Peter Coyne <61690004+petercoyne@users.noreply.github.com>
2022-02-22 13:33:46 +0000

    small tweaks

--- src/lib/TabLink.svelte
+++ src/lib/TabLink.svelte
@@ -2,4 +2,4 @@
 	export let tabName, currentTab
 </script>
 
-<li class="p-4" class:link="{currentTab != tabName}" on:click={() => currentTab = tabName}><h4>{tabName}</h4></li>
+<li class="p-4 md:p-8" class:link="{currentTab != tabName}" on:click={() => currentTab = tabName}><h4>{tabName}</h4></li>
